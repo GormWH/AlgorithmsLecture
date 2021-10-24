92d89b995f28a8b58e62957a2606a431c86cfc02
SuHong Park <sp4kid@gmail.com>
2021-10-24 16:58:55 +0900

    Deque initialized

--- Deque.java
+++ Deque.java
@@ -4,105 +4,35 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
-import edu.princeton.cs.algs4.StdOut;
-
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-    private class Node {
-        Item item;
-        Node before;
-        Node next;
-    }
-
-    private Node first = null;
-    private Node last = null;
 
     // construct an empty deque
-    public Deque() {
-
-    }
+    public Deque()
 
     // is the deque empty?
-    public boolean isEmpty() {
-        return first == null;
-    }
+    public boolean isEmpty()
 
     // return the number of items on the deque
-    public int size() {
-        int size = 0;
-        Node cur = first;
-        while (cur != null) {
-            cur = cur.next;
-            size++;
-        }
-        return size;
-    }
+    public int size()
 
     // add the item to the front
-    public void addFirst(Item item) {
-        first.before = new Node();
-        first = first.before;
-        first.item = item;
-    }
+    public void addFirst(Item item)
 
     // add the item to the back
-    public void addLast(Item item) {
-        last.next = new Node();
-        last.next.before = last;
-        last = last.next;
-        last.item = item;
-    }
+    public void addLast(Item item)
 
     // remove and return the item from the front
-    public Item removeFirst() {
-        Item item = first.item;
-        first = first.next;
-        first.before = null;
-        return item;
-    }
+    public Item removeFirst()
 
     // remove and return the item from the back
-    public Item removeLast() {
-        Item item = last.item;
-        last = last.before;
-        last.next = null;
-        return item;
-    }
+    public Item removeLast()
 
     // return an iterator over items in order from front to back
-    public Iterator<Item> iterator() {
-        return new ListIterator();
-    }
-
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
-
-        public boolean hasNext() {
-            return current != null;
-        }
-
-        public Item next() {
-            try {
-                Item item = current.item;
-                current = current.next;
-                return item;
-            }
-            catch (NoSuchElementException e) {
-                StdOut.println("null access of next() of iterator");
-            }
-        }
-    }
+    public Iterator<Item> iterator()
 
     // unit testing (required)
-    public static void main(String[] args) {
-        Deque<Integer> integerDeque = new Deque<Integer>();
-        integerDeque.addFirst(1);
-        integerDeque.addFirst(2);
-        for (int integer : integerDeque)
-            StdOut.println(integer);
-        StdOut.println(integerDeque.size());
-    }
+    public static void main(String[] args)
 
 }
